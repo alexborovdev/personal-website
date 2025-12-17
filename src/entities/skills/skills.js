f58237a1e14a6b63c560d6e9c8d5009f31e7fa96
Alex Borov <o.borovytskyi@gmail.com>
2025-12-17 14:32:12 -0500

    feat: add skills data

---
+++ src/entities/skills/skills.js
@@ -0,0 +1,27 @@
+const skills = {
+	hard: [
+		'HTML (HTML5), JSX',
+		'CSS (CSS3), SASS (SCSS), Animations',
+		'JavaScript (ES6+, OOP, API integration)',
+		'React',
+		'npm, Vite, Project setup',
+		'BEM, FSD, Accessibility, UX',
+		'SEO, Semantic, Open Graph, Sitemap',
+		'Git, GitHub',
+		'Figma, Adobe Illustrator',
+		'Pre Intermediate English level (B1)'
+	],
+	soft: [
+		'Responsibility & discipline',
+		'Attention to detail',
+		'Adaptability & fast learning',
+		'Time management and task planning',
+		'Communication, openness to feedback',
+		'Stress tolerance & conflict resolution',
+		'Code quality standards',
+		'Clear documentation',
+		'Continuous learning mindset'
+	]
+}
+
+export default skills
