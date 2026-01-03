6ed7fb36360f27aac79d05fabea5726b69ab6b23
Alex Borov <o.borovytskyi@gmail.com>
2026-01-03 00:07:48 -0500

    feat: add Hero markup and styles

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -1,9 +1,19 @@
+import styles from './Hero.module.scss'
+
 const Hero = () => {
   return (
-    <section id="hero" aria-labelledby="hero-title">
-      <div className="container">
+    <section id="hero" aria-labelledby="hero-title" className={styles.hero}>
+      <div className={`container ${styles.content}`}>
         <h2 id="hero-title" className="visually-hidden">About Me</h2>
-        <div>Hero section</div>
+        <div className={styles.description}>
+          <h3 className={styles.title}><span className="accented">Hello!</span> My name is Alex&nbsp;Borov</h3>
+          <h4 className={styles.subtitle}
+          >
+            I'm a <span className="accented">frontend developer</span>.<br />
+            I can do <span className="accented">something great</span> for you.
+          </h4>
+        </div>
+        <button className={styles.button}>Lets check!</button>
       </div>
     </section>
   )
