29f0ee80db30fd91cdc66f83a22ebcd2a5f30186
Alex Borov <o.borovytskyi@gmail.com>
2026-01-03 12:30:57 -0500

    feat: add typing animation

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -1,22 +1,80 @@
+import { useState } from 'react'
+import { TypeAnimation } from 'react-type-animation'
 import Button from '@/shared/ui/Button'
 import styles from './Hero.module.scss'
 
+const ANIMATION_DURATION_AFTER_TYPING = 3150
+const ANIMATION_DURATION_AFTER_CLICK = 600
+const DELAY_AFTER_ANIMATION_END = 1000
+
 const Hero = () => {
+  const [animationStep, setAnimationStep] = useState<0 | 1 | 2>(0)
+  const [isButtonAnimated, setIsButtonAnimated] = useState(false)
+
+  const animateButton = (duration: number) => {
+    setIsButtonAnimated(true)
+    setTimeout(() => {
+      setIsButtonAnimated(false)
+    }, duration)
+  }
+
   return (
     <section id="hero" aria-labelledby="hero-title" className={styles.hero}>
       <div className={`container ${styles.content}`}>
         <h2 id="hero-title" className="visually-hidden">About Me</h2>
         <div className={styles.description}>
-          <h3 className={styles.title}><span className="accented">Hello!</span> My name is Alex&nbsp;Borov</h3>
-          <h4 className={styles.subtitle}
-          >
-            I'm a <span className="accented">frontend developer</span>.<br />
-            I can do <span className="accented">something great</span> for you.
-          </h4>
+          <h3 className={styles.title}>
+            <TypeAnimation
+              sequence={[
+                'Hello! My name is ',
+                () => setAnimationStep(1),
+              ]}
+              speed={50}
+              cursor={false}
+              repeat={0}
+              wrapper="span"
+            />
+            {animationStep >= 1 && (
+              <TypeAnimation
+                className="accented"
+                sequence={[
+                  'Alex\u00A0Borov',
+                  DELAY_AFTER_ANIMATION_END,
+                  () => setAnimationStep(2),
+                ]}
+                speed={50}
+                cursor={false}
+                repeat={0}
+                wrapper="span"
+              />
+            )}
+          </h3>
+          {animationStep === 2 && (
+            <TypeAnimation
+              className={styles.subtitle}
+              sequence={[
+                'I\'m a frontend developer.\nI can do something great for you.',
+                () => animateButton(ANIMATION_DURATION_AFTER_TYPING),
+                ANIMATION_DURATION_AFTER_TYPING,
+                '',
+                DELAY_AFTER_ANIMATION_END,
+              ]}
+              speed={60}
+              deletionSpeed={80}
+              repeat={Infinity}
+              cursor
+              wrapper="p"
+              style={{ whiteSpace: 'pre-line' }}
+            />
+          )}
         </div>
-        <Button className={styles.button}
-                ariaLabel="Scroll to Portfolio section"
-                title="Scroll to Portfolio section"
+        <Button
+          className={`${styles.button} ${
+            isButtonAnimated ? styles.bounce : ''
+          }`}
+          ariaLabel="Scroll to Portfolio section"
+          title="Scroll to Portfolio section"
+          onClick={() => animateButton(ANIMATION_DURATION_AFTER_CLICK)}
         >
           Let’s check!
         </Button>
