750a4913a4c808ecd3ff4116bd35a002822220d8
Alex Borov <o.borovytskyi@gmail.com>
2026-01-10 10:29:37 -0500

    refactor: split ContactsForm logic into Form component

--- src/components/ContactsForm/ContactsForm.tsx
+++ src/components/ContactsForm/ContactsForm.tsx
@@ -1,14 +1,15 @@
 import useContactsForm from '@/shared/hooks/useContactsForm'
+import Form from '@/components/Form'
 import SuccessMessage from '@/components/SuccessMessage'
-import Field from '@/components/Field'
-import Button from '@/shared/ui/Button'
 import styles from './ContactsForm.module.scss'
 
 type Props = {
   onSubmitSuccess?: () => void
 }
 
-const ContactsForm = ({ onSubmitSuccess }: Props) => {
+const ContactsForm = (props: Props) => {
+  const { onSubmitSuccess } = props
+
   const {
     register,
     handleSubmit,
@@ -30,56 +31,17 @@ const ContactsForm = ({ onSubmitSuccess }: Props) => {
           submitResult === 'success' ? styles.hidden : styles.visible
         }`}
       >
-        <form
-          className={styles.form}
-          noValidate
+        <Form
+          register={register}
+          errors={errors}
+          isSubmitting={isSubmitting}
+          showNameError={showNameError}
+          showEmailError={showEmailError}
+          showMessageError={showMessageError}
+          shakeFields={shakeFields}
           onSubmit={handleSubmit(onFormSubmit, onFormError)}
-        >
-          <div className={styles.info}>
-            <Field
-              name="name"
-              label="Your Name"
-              placeholder="John Smith"
-              autoComplete="name"
-              register={register}
-              showError={showNameError}
-              error={errors.name?.message}
-              shake={shakeFields.name}
-            />
-            <Field
-              name="email"
-              label="Your Email"
-              placeholder="example@gmail.com"
-              autoComplete="email"
-              register={register}
-              showError={showEmailError}
-              error={errors.email?.message}
-              shake={shakeFields.email}
-            />
-          </div>
-          <div className={styles.message}>
-            <Field
-              as="textarea"
-              name="message"
-              label="Message"
-              placeholder="Your message"
-              register={register}
-              showError={showMessageError}
-              error={errors.message?.message}
-              shake={shakeFields.message}
-            />
-          </div>
-          <Button
-            type="submit"
-            className={`tileHover ${styles.button}`}
-            title="Sand me an email"
-            disabled={isSubmitting || submitResult === 'success'}
-          >
-            {isSubmitting ? 'Sending…' : 'Send message'}
-          </Button>
-        </form>
+        />
       </div>
-
       <div
         className={`${styles.successLayer} ${
           submitResult === 'success' ? styles.visible : styles.hidden
