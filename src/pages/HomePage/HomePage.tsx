9d026b738e3d8302bfc8575cbfe8ebc5c6cc74f1
Alex Borov <o.borovytskyi@gmail.com>
2026-01-11 20:04:15 -0500

    feat: add 404 page and redirect logic

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,17 +1,21 @@
+import Header from '@/sections/Header'
 import Hero from '@/sections/Hero'
 import Skills from '@/sections/Skills'
 import Portfolio from '@/sections/Portfolio'
 import Contacts from '@/sections/Contacts'
+import Footer from '@/sections/Footer'
 
-const Home = () => {
+const HomePage = () => {
   return (
     <main>
+      <Header />
       <Hero />
       <Skills />
       <Portfolio />
       <Contacts />
+      <Footer />
     </main>
   )
 }
 
-export default Home
+export default HomePage
