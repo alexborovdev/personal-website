4f45964e6d761a40878eed07c2db4be4977dac4a
Alex Borov <o.borovytskyi@gmail.com>
2026-01-12 22:27:28 -0500

    fix: move header and footer out of main

--- src/app/layouts/MainLayout.tsx
+++ src/app/layouts/MainLayout.tsx
@@ -3,6 +3,8 @@ import { useTheme } from '@/shared/providers/ThemeProvider'
 import { useAnimations } from '@/shared/providers/AnimationsProvider'
 import ParticlesBackground from '@/components/ParticlesBackground'
 import ScrollBar from '@/components/ScrollBar'
+import Header from '@/sections/Header'
+import Footer from '@/sections/Footer'
 
 const MainLayout = () => {
   const { theme } = useTheme()
@@ -15,7 +17,9 @@ const MainLayout = () => {
         animations={animations}
       />
       <ScrollBar />
+      <Header />
       <Outlet />
+      <Footer />
     </>
   )
 }
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,19 +1,15 @@
-import Header from '@/sections/Header'
 import Hero from '@/sections/Hero'
 import Skills from '@/sections/Skills'
 import Portfolio from '@/sections/Portfolio'
 import Contacts from '@/sections/Contacts'
-import Footer from '@/sections/Footer'
 
 const HomePage = () => {
   return (
     <main>
-      <Header />
       <Hero />
       <Skills />
       <Portfolio />
       <Contacts />
-      <Footer />
     </main>
   )
 }
